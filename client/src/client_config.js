fc647af3f9c122fe2c21976a0821d92f55b61f5e
Mahammad Zeynalov <magamed9r2.2011@gmail.com>
2020-09-01 10:26:50 +0400

    mvp

---
+++ client/src/client_config.js
@@ -0,0 +1,18 @@
+import {
+    ApolloClient,
+    InMemoryCache
+} from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws'
+
+const link = new WebSocketLink({
+    uri: 'ws://localhost:4000/',
+    options: {
+        reconnect: true
+    }
+});
+
+export default new ApolloClient({
+    link,
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache()
+});
